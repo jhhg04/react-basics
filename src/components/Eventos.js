d249755f63fbe4081c3ee8cacd32ede3dc9eda27
jhhg0 <jhhg04@gmail.com>
2021-06-08 15:22:52 -0500

    Create Component Events

---
+++ src/components/Eventos.js
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+
+export class EventosES6 extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            contador:0
+        }
+        this.sumar = this.sumar.bind(this)
+        this.restar = this.restar.bind(this)
+    }
+
+    sumar(e){
+        console.log("Sumando")
+        console.log(this)
+        this.setState({
+            contador: this.state.contador +1
+        })
+    }
+
+    restar(e){
+        console.log("REstando")
+        console.log(this)
+        this.setState({
+            contador: this.state.contador -1
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>Eventos en Componentes de clase ES6</h2>
+                <nav>
+                    <button onClick={this.sumar}>+</button>
+                    <button onClick={this.restar}>-</button>
+                </nav>
+                <h3>{this.state.contador}</h3>
+            </div>
+        )
+    }
+}
+
+export class EventosES7 extends Component{
+   state = {
+        contador:0
+    }
+
+    sumar = (e) => {
+        console.log("Sumando")
+        console.log(this)
+        this.setState({
+            contador: this.state.contador +1
+        })
+    }
+
+    restar = (e) => {
+        console.log("REstando")
+        console.log(this)
+        this.setState({
+            contador: this.state.contador -1
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>Eventos en Componentes de clase ES7</h2>
+                <nav>
+                    <button onClick={this.sumar}>+</button>
+                    <button onClick={this.restar}>-</button>
+                </nav>
+                <h3>{this.state.contador}</h3>
+            </div>
+        )
+    }
+}
+
+export class MasSobreEventos extends Component{
+     render(){
+         return(
+             <div>
+                 <h2>Mas Sobre Eventos</h2>
+             </div>
+         )
+     }
+ }

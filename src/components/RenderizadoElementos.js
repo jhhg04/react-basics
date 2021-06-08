ecdd43a47058426f9d257c08543b3554301ac84d
jhhg0 <jhhg04@gmail.com>
2021-06-08 15:23:46 -0500

    Create Component RenElemts

---
+++ src/components/RenderizadoElementos.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import data from "../helpers/data.json";
+
+function ElementoLista(props){
+   return(
+     <li>
+        <a href={props.e.web} target="_blank" rel="noreferrer">{props.e.name}</a>
+     </li>
+   )
+}
+
+export default class RenderizadoElementos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seasons: ["Spring", "Summer", "Fall", "Winter"],
+    };
+  }
+
+  render() {
+    console.log(data);
+    return (
+      <div>
+        <h2>Renderizado de Elementos</h2>
+        <h3>Estaciones del año</h3>
+        <ol>
+          {this.state.seasons.map((e, i) => (
+            <li key={i}>{e}</li>
+          ))}
+        </ol>
+        <h3>Frameworks Frontend Js</h3>
+        <ul>
+          {data.frameworks.map((e) => (
+            <ElementoLista key={e.id} e={e}/>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
